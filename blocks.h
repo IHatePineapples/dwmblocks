52b9114a00a00e2265e7ac1eee241f2efff74015
Big Pineapple <lostinhollywood@duck.com>
2021-12-03 19:53:24 +0000

    add more options

---
+++ blocks.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+//	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{" ", "",		60,		0},
+	{"BAT ", "acpi -b | awk '/^Battery/ { print $5 }' | sed 's/,$/\ /' ",		60,		0},
+    {"", "free -h | awk '/^Mem/ { print $4 }'",        10,     0},   
+    {"", "free -h | awk '/^Mem/ { print $3 }'",        10,     0},    
+    {"", "date +'%Y-%m-%d %H:%M:%S'",					5,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
